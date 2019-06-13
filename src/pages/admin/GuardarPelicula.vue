4c1d41029a61ec1770b9d4115b9df218c2880db0
jpolstre <jpolstre@gmail.com>
2019-06-13 19:50:26 -0400

    the mtb add form in save film

--- src/pages/admin/GuardarPelicula.vue
+++ src/pages/admin/GuardarPelicula.vue
@@ -1,12 +1,132 @@
 <template>
 	<q-page padding>
 		<!--  style="height:calc(100vh - 170px);" class="scroll" -->
-				<q-card-section>
+
+
+
+		form para nuevo anterior
+			<q-form
+				@submit="onSubmit"
+				@reset="onReset"
+				class="row"
+			>
+		
+			<div class="col-xs-12 col-sm-12 col-md-5" align="center">
+
+				<q-uploader
+					dense
+					hide-upload-btn
+					url="http://localhost:4444/upload"
+					label="Imagenes"
+					multiple
+					accept=".jpg, image/*"
+					style="max-width: 300px"
+
+				/>
+
+			
+			
+			</div>
+
+			<div class="col-xs-12 col-sm-12 col-md-7" >
+				<div class="q-mt-md">
+
+
+					<q-input
+						dense
+						filled
+						v-for="el, index in form"
+						:key="index"
+						v-model="form.id"
+						:label="el *"
+						lazy-rules
+						:rules="[ val => val && val.length > 0 || 'El id es obligatorio']"
+					/>
+
+					<q-input
+					dense
+					filled
+					v-model="form.title"
+					label="Titulo *"
+					lazy-rules
+					:rules="[ val => val && val.length > 0 || 'El titulo es obligatorio']"
+					/>
+
+					<q-input
+					dense
+					filled
+					v-model="form.original_title"
+					label="Titulo Original *"
+					lazy-rules
+					:rules="[ val => val && val.length > 0 || 'El titulo original es obligatorio']"
+					/>
+
+					<q-input
+					dense
+					filled
+					type="number"
+					v-model="form.anio"
+					label="Año *"
+					lazy-rules
+					:rules="[
+					val => val !== null && val !== '' || 'El Año es obligatorio',
+					val => val > 1999 && val < 2020 || 'Rango: 2000-2019'
+					]"
+					/>
+
+					<q-input
+					dense
+					filled
+					type="textarea"
+					v-model="form.sinopsis"
+					label="Sinopsis *"
+					lazy-rules
+					:rules="[ val => val && val.length > 0 || 'La sinopsis es obligatoria']"
+					/>
+
+					<q-input
+					dense
+					filled
+					type="textarea"
+					v-model="form.detallestecnicos"
+					label="Detalles Tecnicos Del Ripeado*"
+					lazy-rules
+					:rules="[ val => val && val.length > 0 || 'Los detalles tecnicos son obligatorios']"
+					/>
+
+					<q-toggle v-model="form.estado" label="Habilitada" />
+
+			
+					<div class="q-mt-xl">
+						<q-btn label="Guardar" type="submit" color="primary"/>
+						<q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+					</div>
+				</div>
+			</div>
+		</q-form>
+
+		<q-dialog v-model="dialogSearch"  class="scroll-none" @hide="()=>{
+			loadingState=false
+			// searchText = null
+			// resultados = null
+			// swfilm = false
+		}" @show="()=>{
+
+			$refs.inputSearch.focus()
+		}">
+		<q-card style="width: 800px; max-width: 90vw;">
+			<q-card-section>
+				<div class="text-h6" align="center">Buscar en Tmdb</div>
+			</q-card-section>
+
+			<q-separator />
+				<q-card-section style="height:calc(100vh - 170px);" class="scroll">
 					<q-form
 						@submit="onSearch"
 						class="q-gutter-md"
 						>
 						<q-input
+							dense
 							ref ='inputSearch' 
 							standout="bg-primary text-white"
 							v-model.trim="searchText"
@@ -26,11 +146,11 @@
 					<div>
 							<q-list bordered separator>
 							<q-item-label header>{{totalResults}} resultados (elije uno):</q-item-label>
-							<q-item v-for="resultado, index in resultados" :key="index">
+							<q-item v-for="resultado, index in sortResults" :key="index">
 								<q-item-section top avatar>
-									<q-avatar rounded>
+									<q-avatar class="q-ma-none q-pa-none" size="92px" square>
 										{{index}}
-										<img :src="`${$store.state.movie.baseTmdbImages}w342/${resultado.poster_path}`" v-if="resultado.poster_path">
+										<img  :src="`${$store.state.movie.baseTmdbImages}w342/${resultado.poster_path}`" v-if="resultado.poster_path">
 									</q-avatar>
 								</q-item-section>
 								<q-item-section>
@@ -62,11 +182,15 @@
 					</div>
 				</q-card-section>
 				<!-- <q-separator /> -->
-		<!-- 		<q-card-actions align="right">
+				<q-card-actions align="right">
 					<q-btn flat label="Cerrar" icon="close" color="primary" v-close-popup />
-					<q-btn flat label="Accept" color="primary" v-close-popup />
-				</q-card-actions> -->
+					<!-- <q-btn flat label="Accept" color="primary" v-close-popup /> -->
+				</q-card-actions>
 			</q-card>
+		</q-dialog>
+		<q-page-sticky position="bottom-right" :offset="[18, 18]">
+			<q-btn fab icon="search" color="accent"  @click="dialogSearch = true"/>
+		</q-page-sticky>
 	</q-page>
 </template>
 <script>
@@ -75,14 +199,41 @@
 	export default{
 		data(){
 			return{
+				dialogSearch:true,
 				searchText:null,
 				loadingState:false,
 				resultados:[], 
 				totalPages:0,
 				totalResults:0,
+				form:{//solo los datos que se muestran en la lista de peliculas.
+					id: null,
+					vote_count: null,
+					vote_average: null,
+					title: null,
+					popularity: null,
+					poster_path: null,
+					original_language: null,
+					original_title: null,
+					backdrop_path: null,
+					overview: null,
+					release_date: null
+				}
 			}
 		},
-		methods: {
+		
+	computed:{
+		sortResults(){
+			return this.resultados.sort(function(a, b){
+					var keyA = new Date(a.release_date),
+							keyB = new Date(b.release_date);
+					// Compare the 2 dates
+					if(keyA > keyB) return -1;
+					if(keyA < keyB) return 1;
+					return 0;
+			});
+		}
+	},
+	methods: {
 
 			onLoad(index, done){
 
@@ -123,6 +274,7 @@
 			},
 
 			onSearch(){
+
 				// console.log(this.searchText)
 				if(!this.searchText){
 					this.$q.notify({
@@ -134,6 +286,7 @@
 					this.searchText = null
 
 				}else{
+					this.resultados = []
 					this.loadingState = true
 					const this_ = this
 
